b8c9fcac40ee7784ddda33bd73a80d1d3ea07b5b
ShunYaoTeo <2101104@sit.singaporetech.edu.sg>
2023-06-23 13:13:44 +0800

    bat

--- src/central_manifests/apply_manifests.bat
+++ src/central_manifests/apply_manifests.bat
@@ -22,29 +22,28 @@ for /R "%~1" %%f in (%~2) do (
 goto :eof
 
 :: Apply DB manifests in order
-call :applyManifests %BASE_PATH%\auth_manifests\auth_db_manifests db-auth-configmap.yaml
-call :applyManifests %BASE_PATH%\auth_manifests\auth_db_manifests db-auth-pvc.yaml
-call :applyManifests %BASE_PATH%\auth_manifests\auth_db_manifests db-secrets.yaml
-call :applyManifests %BASE_PATH%\auth_manifests\auth_db_manifests db-auth-deploy.yaml
-call :applyManifests %BASE_PATH%\auth_manifests\auth_db_manifests db-auth-service.yaml
+call :applyManifests "%BASE_PATH%\auth_manifests\auth_db_manifests" db-auth-configmap.yaml
+call :applyManifests "%BASE_PATH%\auth_manifests\auth_db_manifests" db-auth-pvc.yaml
+call :applyManifests "%BASE_PATH%\auth_manifests\auth_db_manifests" db-secrets.yaml
+call :applyManifests "%BASE_PATH%\auth_manifests\auth_db_manifests" db-auth-deploy.yaml
+call :applyManifests "%BASE_PATH%\auth_manifests\auth_db_manifests" db-auth-service.yaml
 
-call :applyManifests %BASE_PATH%\ewaste_manifests\ewaste_db_manifests db-ewaste-configmap.yaml
-call :applyManifests %BASE_PATH%\ewaste_manifests\ewaste_db_manifests db-ewaste-pvc.yaml
-call :applyManifests %BASE_PATH%\ewaste_manifests\ewaste_db_manifests db-secrets.yaml
-call :applyManifests %BASE_PATH%\ewaste_manifests\ewaste_db_manifests db-ewaste-deploy.yaml
-call :applyManifests %BASE_PATH%\ewaste_manifests\ewaste_db_manifests db-ewaste-service.yaml
+call :applyManifests "%BASE_PATH%\ewaste_manifests\ewaste_db_manifests" db-ewaste-configmap.yaml
+call :applyManifests "%BASE_PATH%\ewaste_manifests\ewaste_db_manifests" db-ewaste-pvc.yaml
+call :applyManifests "%BASE_PATH%\ewaste_manifests\ewaste_db_manifests" db-secrets.yaml
+call :applyManifests "%BASE_PATH%\ewaste_manifests\ewaste_db_manifests" db-ewaste-deploy.yaml
+call :applyManifests "%BASE_PATH%\ewaste_manifests\ewaste_db_manifests" db-ewaste-service.yaml
 
-call :applyManifests %BASE_PATH%\rewards_manifests\rewards_db_manifests db-rewards-configmap.yaml
-call :applyManifests %BASE_PATH%\rewards_manifests\rewards_db_manifests db-rewards-pvc.yaml
-call :applyManifests %BASE_PATH%\rewards_manifests\rewards_db_manifests db-secrets.yaml
-call :applyManifests %BASE_PATH%\rewards_manifests\rewards_db_manifests db-rewards-deploy.yaml
-call :applyManifests %BASE_PATH%\rewards_manifests\rewards_db_manifests db-rewards-service.yaml
+call :applyManifests "%BASE_PATH%\rewards_manifests\rewards_db_manifests" db-rewards-configmap.yaml
+call :applyManifests "%BASE_PATH%\rewards_manifests\rewards_db_manifests" db-rewards-pvc.yaml
+call :applyManifests "%BASE_PATH%\rewards_manifests\rewards_db_manifests" db-secrets.yaml
+call :applyManifests "%BASE_PATH%\rewards_manifests\rewards_db_manifests" db-rewards-deploy.yaml
+call :applyManifests "%BASE_PATH%\rewards_manifests\rewards_db_manifests" db-rewards-service.yaml
 
 :: Apply service manifests
-call :applyManifests %BASE_PATH%\gateway_manifests "*.yaml"
-call :applyManifests %BASE_PATH%\auth_manifests "*.yaml"
-call :applyManifests %BASE_PATH%\ewaste_manifests "*.yaml"
-call :applyManifests %BASE_PATH%\rewards_manifests "*.yaml"
-
+call :applyManifests "%BASE_PATH%\gateway_manifests" *.yaml
+call :applyManifests "%BASE_PATH%\auth_manifests" *.yaml
+call :applyManifests "%BASE_PATH%\ewaste_manifests" *.yaml
+call :applyManifests "%BASE_PATH%\rewards_manifests" *.yaml
 
 exit /b 0
