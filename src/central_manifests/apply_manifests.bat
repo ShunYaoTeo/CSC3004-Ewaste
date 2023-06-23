202438565ec62adc86e6b1ab598447b8785b5695
ShunYaoTeo <2101104@sit.singaporetech.edu.sg>
2023-06-23 12:28:52 +0800

    changed sh to batch file

---
+++ src/central_manifests/apply_manifests.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal
+
+:: Set your base path here, or pass as a command line argument
+set BASE_PATH=%~1
+if "%BASE_PATH%"=="" (
+    echo Base path not set. Please pass the base path as a command line argument.
+    exit /b 1
+)
+
+:: Function to apply manifests
+:applyManifests
+for %%f in (%BASE_PATH%\%~1\*.yaml) do (
+    kubectl apply -f %%f
+)
+exit /b 0
+
+:: Apply DB manifests in order
+call :applyManifests auth_manifests\auth_db_manifests\db-auth-configmap.yaml
+call :applyManifests auth_manifests\auth_db_manifests\db-auth-pvc.yaml
+call :applyManifests auth_manifests\auth_db_manifests\db-secrets.yaml
+call :applyManifests auth_manifests\auth_db_manifests\db-auth-deploy.yaml
+call :applyManifests auth_manifests\auth_db_manifests\db-auth-service.yaml
+
+call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-configmap.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-pvc.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests\db-secrets.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-deploy.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-service.yaml
+
+call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-configmap.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-pvc.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests\db-secrets.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-deploy.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-service.yaml
+
+:: Apply service manifests
+call :applyManifests gateway_manifests
+call :applyManifests auth_manifests
+call :applyManifests ewaste_manifests
+call :applyManifests rewards_manifests
