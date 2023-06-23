9a7a3d9722eb571b798a69d2037cc86c49e69047
ShunYaoTeo <2101104@sit.singaporetech.edu.sg>
2023-06-23 12:41:33 +0800

    Update batch file

--- src/central_manifests/apply_manifests.bat
+++ src/central_manifests/apply_manifests.bat
@@ -10,32 +10,32 @@ if "%BASE_PATH%"=="" (
 
 :: Function to apply manifests
 :applyManifests
-for %%f in (%BASE_PATH%\%~1\*.yaml) do (
+for %%f in (%BASE_PATH%\%~1\%~2) do (
     kubectl apply -f %%f
 )
 exit /b 0
 
 :: Apply DB manifests in order
-call :applyManifests auth_manifests\auth_db_manifests\db-auth-configmap.yaml
-call :applyManifests auth_manifests\auth_db_manifests\db-auth-pvc.yaml
-call :applyManifests auth_manifests\auth_db_manifests\db-secrets.yaml
-call :applyManifests auth_manifests\auth_db_manifests\db-auth-deploy.yaml
-call :applyManifests auth_manifests\auth_db_manifests\db-auth-service.yaml
+call :applyManifests auth_manifests\auth_db_manifests db-auth-configmap.yaml
+call :applyManifests auth_manifests\auth_db_manifests db-auth-pvc.yaml
+call :applyManifests auth_manifests\auth_db_manifests db-secrets.yaml
+call :applyManifests auth_manifests\auth_db_manifests db-auth-deploy.yaml
+call :applyManifests auth_manifests\auth_db_manifests db-auth-service.yaml
 
-call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-configmap.yaml
-call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-pvc.yaml
-call :applyManifests ewaste_manifests\ewaste_db_manifests\db-secrets.yaml
-call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-deploy.yaml
-call :applyManifests ewaste_manifests\ewaste_db_manifests\db-ewaste-service.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests db-ewaste-configmap.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests db-ewaste-pvc.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests db-secrets.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests db-ewaste-deploy.yaml
+call :applyManifests ewaste_manifests\ewaste_db_manifests db-ewaste-service.yaml
 
-call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-configmap.yaml
-call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-pvc.yaml
-call :applyManifests rewards_manifests\rewards_db_manifests\db-secrets.yaml
-call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-deploy.yaml
-call :applyManifests rewards_manifests\rewards_db_manifests\db-rewards-service.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests db-rewards-configmap.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests db-rewards-pvc.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests db-secrets.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests db-rewards-deploy.yaml
+call :applyManifests rewards_manifests\rewards_db_manifests db-rewards-service.yaml
 
 :: Apply service manifests
-call :applyManifests gateway_manifests
-call :applyManifests auth_manifests
-call :applyManifests ewaste_manifests
-call :applyManifests rewards_manifests
+call :applyManifests gateway_manifests *.yaml
+call :applyManifests auth_manifests *.yaml
+call :applyManifests ewaste_manifests *.yaml
+call :applyManifests rewards_manifests *.yaml
